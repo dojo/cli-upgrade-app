c98737687ce5be3c08af561fb84d8116a78b17ae
Rory Mulligan <rmulligan@sitepen.com>
2020-03-26 11:11:17 -0400

    Updating lint staged

--- src/v6/main.ts
+++ src/v6/main.ts
@@ -8,5 +8,16 @@ export const config: VersionConfig = {
 			name: 'Refactor framework/widget-core to framework/core',
 			path: resolve(__dirname, 'transforms', 'core-refactor.js')
 		}
-	]
+	],
+	dependencies: {
+		add: ['typescript@3.4.5'],
+		remove: [],
+		updateVersion: '^6.0.0'
+	},
+
+	postTransform() {
+		console.log(
+			'\n\nDojo 6 requires TypeScript version 3.4.5. Due to this upgrade, you may need to resolve some compilation errors manually.\n\n'
+		);
+	}
 };
