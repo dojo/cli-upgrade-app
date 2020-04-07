84197b79dd23d862606834a3e34d2bc232165306
Bradley Maier <bmaier@sitepen.com>
2020-04-07 11:37:12 -0700

    Warn if the router injector is used and we can't find a routes file

--- src/v7/transforms/route-ids.ts
+++ src/v7/transforms/route-ids.ts
@@ -1,13 +1,18 @@
 import { getLineEndings } from '../../util';
+import * as path from 'path';
+import chalk from 'chalk';
+import { warning } from 'log-symbols';
 
 export default function(file: any, api: any) {
 	let quote: string | undefined;
+	const routesRegex = /src\/routes\.ts(x)?$/;
+	const j = api.jscodeshift;
+	const jFile = j(file.source);
 
-	if (file.path && /src\/routes\.ts(x)?$/.test(file.path)) {
-		const j = api.jscodeshift;
+	if (file.path && routesRegex.test(file.path)) {
 		const lineTerminator = getLineEndings(file.source);
 
-		return j(file.source)
+		return jFile
 			.find(j.ObjectExpression)
 			.replaceWith((p: any) => {
 				if (p.node && p.node.properties) {
@@ -33,5 +38,36 @@ export default function(file: any, api: any) {
 				return p;
 			})
 			.toSource({ quote: quote || 'single', lineTerminator });
+	} else {
+		debugger;
+		const injectorPaths = jFile
+			.find(j.ImportDeclaration)
+			.filter((path: any) => path.node.source.value === '@dojo/framework/routing/RouterInjector')
+			.paths();
+		const injectorImports = injectorPaths && injectorPaths[0];
+		const injectorLocalImport =
+			injectorImports &&
+			injectorImports.value.specifiers.find(({ imported: { name } }: any) => name === 'registerRouterInjector');
+		const localName = injectorLocalImport && injectorLocalImport.local.name;
+		if (jFile.find(j.CallExpression, { callee: { name: localName } }).length) {
+			const routesImports = jFile
+				.find(j.ImportDeclaration)
+				.filter((path: any) => path.node.source.value.endsWith('routes'))
+				.paths();
+			if (
+				!routesImports ||
+				!routesImports.some((routesImport: any) => {
+					debugger;
+					const routesPath = `${path.resolve(file.path, `../${routesImport.node.source.value}`)}.ts`;
+					return routesRegex.test(routesPath);
+				})
+			) {
+				console.log(
+					`${chalk.bold.yellow(
+						warning
+					)}  The routing config passed to the router injector could not be located. It needs to be updated to include 'id' properties`
+				);
+			}
+		}
 	}
 }
--- tests/unit/v7/transforms/route-ids.ts
+++ tests/unit/v7/transforms/route-ids.ts
@@ -1,6 +1,7 @@
-const { describe, it } = intern.getInterface('bdd');
+const { describe, it, beforeEach, afterEach } = intern.getInterface('bdd');
 const { assert } = intern.getPlugin('chai');
 import { EOL } from 'os';
+import * as sinon from 'sinon';
 
 const normalizeLineEndings = (str: string) => str.replace(/\r?\n/g, EOL);
 
@@ -88,5 +89,39 @@ export default [
 	it('does not run against other files', () => {
 		const output = moduleTransform({ ...input, path: 'src/mailroutes.tsx' }, { jscodeshift, stats: () => {} });
 		assert.isUndefined(output);
+	});
+
+	describe('warning', () => {
+		let sandbox: sinon.SinonSandbox;
+		let loggerStub: sinon.SinonStub;
+
+		beforeEach(() => {
+			sandbox = sinon.sandbox.create();
+			loggerStub = sandbox.stub(console, 'log');
+		});
+
+		afterEach(() => {
+			sandbox.restore();
+		});
+
+		it('should log if the router is used but the config file will not be updated', () => {
+			const source = normalizeLineEndings(`
+				import { registerRouterInjector } from '@dojo/framework/routing/RouterInjector';
+				import routes from './someotherroutes';
+				registerRouterInjector(routes as any, null as any);
+			`);
+			moduleTransform({ source, path: 'src/main.ts' }, { jscodeshift, stats: () => {} });
+			assert.isTrue(loggerStub.calledOnce);
+		});
+
+		it('should not log if the router is used and the config file will be updated', () => {
+			const source = normalizeLineEndings(`
+				import { registerRouterInjector } from '@dojo/framework/routing/RouterInjector';
+				import routes from './routes';
+				registerRouterInjector(routes as any, null as any);
+			`);
+			moduleTransform({ source, path: 'src/main.ts' }, { jscodeshift, stats: () => {} });
+			assert.isFalse(loggerStub.called);
+		});
 	});
 });
